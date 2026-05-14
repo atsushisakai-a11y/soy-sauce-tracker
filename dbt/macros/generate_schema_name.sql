3f1b95f2785589d395c31a5d26e96c3b7f88ecbf
Atsushi <atsushi@mac.home>
2026-05-14 11:56:04 +0200

    fix: add dispatched macro name for dbt 1.9+ schema override

--- dbt/macros/generate_schema_name.sql
+++ dbt/macros/generate_schema_name.sql
@@ -5,3 +5,11 @@
         {{ custom_schema_name | trim }}
     {%- endif -%}
 {%- endmacro %}
+
+{% macro kikkoman_price_tracker__generate_schema_name(custom_schema_name, node) -%}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}
+    {%- else -%}
+        {{ custom_schema_name | trim }}
+    {%- endif -%}
+{%- endmacro %}
