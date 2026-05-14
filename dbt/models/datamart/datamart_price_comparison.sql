033247f9726e2ac4d42c11f85697e488c2fc70cf
Atsushi <atsushi@mac.home>
2026-05-14 14:41:44 +0200

    Fix stale fact_price tests and datamart shop_count join

--- dbt/models/datamart/datamart_price_comparison.sql
+++ dbt/models/datamart/datamart_price_comparison.sql
@@ -1,12 +1,14 @@
 {{ config(materialized='view') }}
 
 SELECT
-    brand,
-    product_variant         AS product_name,
-    shop_count,
-    MIN(price_eur)          AS min_price_eur,
-    MAX(price_eur)          AS max_price_eur,
-    AVG(price_eur)          AS avg_price_eur
+    d.brand,
+    d.product_variant         AS product_name,
+    COUNT(DISTINCT d.shop_name) AS shop_count,
+    MIN(f.price_eur)          AS min_price_eur,
+    MAX(f.price_eur)          AS max_price_eur,
+    AVG(f.price_eur)          AS avg_price_eur
 
-FROM {{ ref('fact_price') }}
-GROUP BY 1, 2, 3
+FROM {{ ref('fact_price') }}  f
+JOIN {{ ref('dim_price') }}   d  ON f.product_id = d.product_id
+
+GROUP BY 1, 2
